8a4c8faaba6d52bf49c83f6cc50c67c808a73b9d
SepehrYari <91702741+SepehrYari@users.noreply.github.com>
2024-02-28 03:43:26 -0800

    I made a dumb move
    I should have made these changes in main/dev branch but i didn't and now I am sorry since we will have to merge the changes I made in this

--- Assets/Script/AnxityMeter.cs
+++ Assets/Script/AnxityMeter.cs
@@ -6,24 +6,47 @@ using TMPro;
 
 public class AnxityMeter : MonoBehaviour
 {
-    private float score = 0f;
+    public float score = 0f;
     public TextMeshProUGUI anxityScore;
+    public float TeaRestore;
 
     private float timePasted = 0f;
-    public TextMeshProUGUI Timer;
+    public TextMeshProUGUI MinTimer;
+    public TextMeshProUGUI SecTimer;
 
+    public Player Player;
+
+    public int seconds = 0;
+    public int min = 0;
+    Coroutine Timerate;
 
     void Start()
     {
         // Invoke the function to increase score every 1 second (change the second parameter as needed)
         InvokeRepeating("IncreaseScore", 0f, 1f);
+
+
+        //coroutine
+        Timerate = StartCoroutine(TimeCounter());
+
+
+        //we get the player script from somewhere from the scene
+        Player = FindAnyObjectByType<Player>();
+
     }
 
     void Update()
     {
         anxityScore.text = score.ToString("0.0");
-        timePasted += Time.deltaTime;
-        Timer.text = timePasted.ToString("0:00");
+
+        if (min == 5 && seconds == 59 || score >= 100)
+        {
+            //end game
+            Player.GameOver = true;
+            Player.canMove = false;
+
+        }
+
 
     }
 
@@ -41,8 +64,33 @@ public class AnxityMeter : MonoBehaviour
             score += 0.1f;
 
         }
-        Debug.Log("Current Score: " + score);
+        //Debug.Log("Current Score: " + score);
     }
+
+    private IEnumerator TimeCounter()
+    {
+        
+        while (seconds <= 59)
+        {
+            seconds++;
+            if (seconds == 60)
+            {
+                min++;
+                seconds = 00;
+
+                
+            }
+
+           
+            
+            MinTimer.text = min.ToString("0");
+            SecTimer.text = seconds.ToString(":00");
+            yield return new WaitForSeconds(1);
+        }
+
+    }
+
+
 
 
 }
