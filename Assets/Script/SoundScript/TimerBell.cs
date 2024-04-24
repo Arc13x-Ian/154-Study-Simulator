b2cfb838af95dff15d22043a3becfcdedddcdac8
CongSangDao <sangdaovnb2002@gmail.com>
2024-04-24 01:31:13 -0700

    James Update 7
    Add more sound

---
+++ Assets/Script/SoundScript/TimerBell.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerBell : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private float timerDuration = 300.0f; 
+    private float timeElapsed = 0.0f;
+    private bool hasPlayed = false; 
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!hasPlayed) 
+        {
+            timeElapsed += Time.deltaTime; 
+
+            if (timeElapsed >= timerDuration)
+            {
+                if (audioSource && !audioSource.isPlaying)
+                {
+                    audioSource.Play(); 
+                    hasPlayed = true; 
+                }
+            }
+        }
+    }
+}
